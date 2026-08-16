f3521587bdff3eba9848f4232898090382a4d106
Pedro Lucca <pedro.lucca.ramos.10@gmail.com>
2026-08-16 17:38:40 -0300

    variaveis de  acesso ao bd

---
+++ config/data_base.php
@@ -0,0 +1,23 @@
+<?php
+class Database {
+    private static $host = 'localhost';
+    private static $dbname = 'biblioteca_db';
+    private static $username = 'root';
+    private static $password = '';
+    private static $pdo = null;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
