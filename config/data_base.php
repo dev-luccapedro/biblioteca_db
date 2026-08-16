a6a6a1c254faf379fbeae3a2af6ba0bc5b394559
Pedro Lucca <pedro.lucca.ramos.10@gmail.com>
2026-08-16 18:02:29 -0300

    criando objeto PDO para conexao e forticando a estrutura caso dê erro

--- config/data_base.php
+++ config/data_base.php
@@ -6,6 +6,24 @@ class Database {
     private static $password = '';
     private static $pdo = null;
 
+ public static function getConnection() {
+        if (self::$pdo === null) {
+            try {
+                self::$pdo = new PDO(
+                    "mysql:host=" . self::$host . ";dbname=" . self::$dbname . ";charset=utf8mb4",
+                    self::$username,
+                    self::$password,
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                    ]
+                );
+            } catch (PDOException $e) {
+                die("Erro de conexão com o banco de dados: " . $e->getMessage());
+            }
+        }
+        return self::$pdo;
+    }
 
 
 
