d9cf57cf8f4de5629df2a278cb298c6b39c7c1c3
Pedro Lucca <pedro.lucca.ramos.10@gmail.com>
2026-08-17 20:46:15 -0300

    busca a conexão PDO e guarda

--- src/Repositories/LivroRepository.php
+++ src/Repositories/LivroRepository.php
@@ -1,3 +1,28 @@
 <?php
 require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/../Entities/Livro.php';
+
+
+ class LivroRepository {
+     private PDO $db;
+ 
+     public function __construct() {
+         $this->db = Database::getConnection();
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
