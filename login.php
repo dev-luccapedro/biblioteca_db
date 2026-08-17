9a126d2ad76f9a16b0ebb2ecd4aca8cfb91a65b6
Pedro Lucca <pedro.lucca.ramos.10@gmail.com>
2026-08-17 19:31:39 -0300

    propriedades dos livros

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+class Livro {
+    private ?int $id = null;
+    private string $titulo;
+    private string $isbn;
+    private int $categoriaId;
+    private string $status;
+    private ?string $imagem = null;
+    private array $autoresIds = [];
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
