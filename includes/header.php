659074570683e60fa15df92fdec71ad5846e6141
Pedro Lucca <pedro.lucca.ramos.10@gmail.com>
2026-08-18 20:19:26 -0300

    exibindo o topo do site com o nome do usuário logado

--- includes/header.php
+++ includes/header.php
@@ -13,8 +13,25 @@
 
 ?>
 
-
-
+ <!DOCTYPE html>
+ <html lang="pt-br">
+ <head>
+     <meta charset="UTF-8">
+     <title>Sistema de Biblioteca Online</title>
+     
+ </head>
+ <body>
+ <?php if (isset($_SESSION['usuario'])): ?>
+ <header>
+     <h2>📚 Biblioteca Online</h2>
+     <nav>
+         <span>Olá, <?= htmlspecialchars($_SESSION['usuario']['nome']) ?></span>
+         <a href="livros.php">Livros</a>
+         <a href="logout.php" style="color: #ff6b6b;">Sair</a>
+     </nav>
+ </header>
+ <?php endif; ?>
+ <div class="container">
 
 
 
