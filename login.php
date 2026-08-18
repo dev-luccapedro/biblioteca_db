b31e37878aa1d30c0aedb00d3f94fdee46770b2b
Pedro Lucca <pedro.lucca.ramos.10@gmail.com>
2026-08-18 20:42:14 -0300

    campos html p colocar informações + rodape

--- login.php
+++ login.php
@@ -25,3 +25,20 @@
 
  require_once __DIR__ . '/includes/header.php';
  ?>
+
+ <h2>Login de Acesso</h2>
+ <?php if ($erro): ?><div class="alert-error"><?= $erro ?></div><?php endif; ?>
+ 
+ <form method="POST">
+     <div class="form-group">
+         <label>E-mail:</label>
+         <input type="email" name="email" required placeholder="admin@biblioteca.com">
+     </div>
+     <div class="form-group">
+         <label>Senha:</label>
+        <input type="password" name="senha" required placeholder="admin123">
+     </div>
+     <button type="submit" class="btn">Entrar</button>
+ </form>
+
+ <?php require_once __DIR__ . '/includes/footer.php'; ?>
