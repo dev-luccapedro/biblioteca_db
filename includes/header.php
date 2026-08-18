9d47348ad5930e508653a7cd51e56f9bdb60509d
Pedro Lucca <pedro.lucca.ramos.10@gmail.com>
2026-08-18 19:50:45 -0300

    controle de segurança

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,16 @@
      session_start();
  }
 
+ $paginasPublicas = ['login.php'];
+ $paginaAtual = basename($_SERVER['PHP_SELF']);
+ 
+ if (!in_array($paginaAtual, $paginasPublicas) && !isset($_SESSION['usuario'])) {
+     header("Location: login.php");
+     exit;
+ }
+
+?>
+
 
 
 
