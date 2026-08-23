6d8fe65f62711d49efab4f7d76c3bad47f0828fb
Pedro Lucca <pedro.lucca.ramos.10@gmail.com>
2026-08-23 13:32:20 -0300

    fim

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <metaz charset="UTF-8">
-    <title>Meu Site PHP e HTML</title>
-       <link rel="stylesheet" href="index.css">
-</head>
-<body>
-    <h1>Bem-vindo ao meu site</h1>
-    <p>Esta parte é apenas HTML puro.</p>
+<?php
+session_start();
 
-    <?php
-        // Aqui começa o código PHP processado pelo servidor
-        $mensagem = "Olá! Este texto foi gerado dinamicamente usando PHP.";
-        echo "<p style='color: red;'>$mensagem</p>";
-    ?>
-</body>
-</html>
+// Se o usuário já estiver logado, vai direto para a lista de livros
+if (isset($_SESSION['usuario'])) {
+    header("Location: livros.php");
+    exit;
+}
+
+// Caso contrário, redireciona para a tela de login
+header("Location: login.php");
+exit;
